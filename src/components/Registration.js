3cb23eb30dc3ccf2ccafde7fb409635b71060e41
akshat-52 <52.akkey@gmail.com>
2022-01-18 18:53:03 +0530

    updated backend URL

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -26,7 +26,7 @@ function Registration() {
 
   //   // userDetails.username = 'nik';
   //  console.log(userDetails);
-      await axios.post('http://localhost:5000/user-register', userDetails)
+      await axios.post('https://bookmyshow-akshat52.herokuapp.com/user-register', userDetails)
       .then((data) => {
         Swal.fire({
           title: `<strong>${data.data.message}</strong>`,
